e747b25f8161627b623dbfd2acb3763bc36e87b1
Pedro <pedrojs.contato@gmail.com>
2022-07-02 20:01:49 -0300

    adding-multer

---
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { ImportCategoryUseCase } from './ImportCategoryUseCase'
+
+class ImportCategoryController {
+  private importCategoryUseCase
+  constructor (importCategoryUseCase: ImportCategoryUseCase) {
+    this.importCategoryUseCase = importCategoryUseCase
+  }
+
+  handle (request: Request, response: Response) {
+    const { file } = request
+    this.importCategoryUseCase.execute(file)
+    return response.send()
+  }
+}
+export { ImportCategoryController }
